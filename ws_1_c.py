c9a2a7c0302a24872aeb8b4a7b3597452c1441d7
ggu <ggu@hphk.kr>
2024-01-10 21:50:40 +0900

    추가실습 c단계

---
+++ ws_1_c.py
@@ -0,0 +1,2 @@
+password = "In the bustling city, where life is a constant race against time, uoy often find yourself wondering if there's a shortcut to success. The vibrant lights of the cityscape illuminate the night, casting shadows on the short-lived dreams of those who seek fortune. As you navigate through the crowded streets, you realize the deen for guidance, like a compass pointing python. You need direction in this chaotic journey called life."
+# 아래에 코드를 작성하시오.
