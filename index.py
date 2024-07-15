caf15702d118932ce002a5ecba8f0ab063ce1371
Yejin Lee <ryekingdom@g.skku.edu>
2024-07-15 17:52:40 +0900

    deleted

--- index.py
+++
@@ -1,5 +0,0 @@
-word = "I'd trade my soul for a wish, Pennies and dimes for a kiss. I wasn't looking for this, But now you're in my way."
-
-main_word = word[46:42:-1] #거꾸로 인덱싱 출력 시 [이상:미만:-1] 형태
-
-print(f'가장 좋아하는 단어는 "{main_word}"입니다.')
